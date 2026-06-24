3b22eb042b3f8cf6caa0cde27cd61bd173debefc
Kshitij Thakre <144704762+kshitij-thakre@users.noreply.github.com>
2026-06-24 12:03:28 +0530

    Time: 1 ms (13.79%), Space: 46.5 MB (61.24%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,11 +1,20 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int[] ans = new int[2 * n];
-        for (int i = 0; i < n; i++) {
-            ans[2 * i] = nums[i];
-            ans[2 * i + 1] = nums[i + n];
+      for(int i =0; i<n; i++){
+            nums[i] = nums[i] + 1024 * nums[i+n];
         }
+                int index = 2*n - 1;
 
-        return ans;
+        for(int i = n - 1; i >= 0; i--) {
+
+            int y = nums[i] / 1024;
+
+            int x = nums[i] % 1024;
+
+            nums[index--] = y;
+
+            nums[index--] = x;
+        }
+        return nums;
     }
 }
